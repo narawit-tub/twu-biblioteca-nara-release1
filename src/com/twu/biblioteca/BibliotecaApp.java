0d9df688a8517a8f08b1412c032ae89e7a10fde9
Narawit Tubtimtoe <narawit.tub@outlook.co.th>
2021-05-10 17:03:53 +0700

    feat: welcome message and show app options

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,8 +1,25 @@
 package com.twu.biblioteca;
 
+import java.util.Scanner;
+
 public class BibliotecaApp {
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        Scanner in = new Scanner(System.in);
+        System.out.print("Welcome! Press 'Enter' Key to continue.");
+
+        // wait use pass "enter" key
+        String tmp = in.nextLine();
+        System.out.println(tmp);
+        System.out.print("\n\t*** What do you want to do\n" +
+                "\t1. View a list of book\n" +
+                "\t2. Check out book\n" +
+                "\t3. Return a book\n" +
+                "\t4. Quit this app\n\n");
+
+    }
+
+    public void showIns(){
+
     }
 }
--- test/com/twu/biblioteca/ExampleTest.java
+++ test/com/twu/biblioteca/ExampleTest.java
@@ -1,13 +1,63 @@
 package com.twu.biblioteca;
 
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
+
 import static org.junit.Assert.assertEquals;
 
 public class ExampleTest {
 
+    public final String WELCOME_MES = "Welcome! Press 'Enter' Key to continue.";
+    public final String OPTIONS_MES = "\n\t*** What do you want to do\n" +
+            "\t1. View a list of book\n" +
+            "\t2. Check out book\n" +
+            "\t3. Return a book\n" +
+            "\t4. Quit this app\n\n";
+
+    public ByteArrayOutputStream outputBytes;
+    public PrintStream outputConsole;
+
+    @Before
+    public void setUp(){
+        outputBytes = new ByteArrayOutputStream();
+        outputConsole = System.out;
+        System.setOut(new PrintStream(outputBytes));
+    }
+
+    @After
+    public void tearDown(){
+        System.setOut(outputConsole);
+    }
+
     @Test
-    public void test() {
-        assertEquals(1, 1);
+    public void UserSeesAWelcomeMessage() {
+        // When
+        BibliotecaApp.main(new String[] {});
+
+        // Then
+        // 1. "welcome" message in terminal
+        // 2. show "press enter key to continue"
+        assertEquals(WELCOME_MES, outputBytes.toString());
+    }
+
+    @Test
+    public void PressEnterToViewMainMenuOfOptions() {
+        // Given
+        InputStream inputConsole = System.in;
+        ByteArrayInputStream inputBytes = new ByteArrayInputStream("\n\n2".getBytes());
+        System.setIn(inputBytes);
+
+        // When
+        BibliotecaApp.main(new String[] {});
+
+        // Then
+        assertEquals(String.format(WELCOME_MES + "\n" + OPTIONS_MES), outputBytes.toString());
     }
 }
