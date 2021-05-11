d23f26577a46b905dad0042565dc5805dba46bc0
Narawit Tubtimtoe <narawit.tub@outlook.co.th>
2021-05-11 15:04:55 +0700

    chore: create a list of book in user-message class

--- src/com/twu/biblioteca/ConsoleMessage.java
+++ src/com/twu/biblioteca/ConsoleMessage.java
@@ -4,8 +4,13 @@ public class ConsoleMessage {
     public final String WELCOME_MES = "Welcome! type 'menu' to continue >>";
     public final String OPTIONS_MES = "\n\t*** What do you want to do\n" +
             "\t1. View a list of book\n" +
+            "\t2. View a list of book (include author and publication year)\n" +
             "\t2. Check out book\n" +
             "\t3. Return a book\n" +
             "\t4. Quit this app\n\n";
     public final String ERROR_MES_REPEAT_USER_OPTION = "Sorry, it isn\'t correct option. please try again >>";
+    public final String SHOW_A_LIST_OF_BOOKS_MES = "-- List of books --" +
+            "\n1. Klara and the Sun by Kazuo Ishiguro" +
+            "\n2. Luster by Raven Leilani" +
+            "\n3. Aftershocks by Nadia Owusu";
 }
